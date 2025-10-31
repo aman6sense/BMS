25aef06995141b82571be4f997ee9b38ef78b284
amanullah7649 <mdamanullah7649@gmail.com>
2025-11-01 03:52:41 +0600

    added missing validation and rename published date

--- src/modules/books/dtos/updateBook.dto.ts
+++ src/modules/books/dtos/updateBook.dto.ts
@@ -1,25 +1,23 @@
 import { Transform } from 'class-transformer';
-import { IsDateString, IsMongoId, IsOptional, IsString, MaxLength } from 'class-validator';
+import { IsDateString, IsISBN, IsMongoId, IsOptional, IsString, MaxLength } from 'class-validator';
 
 /**
  * Data Transfer Object for updating an existing book
  */
 export class UpdateBookDto {
-    @IsString()
-    @IsOptional()
-    @Transform(({ value }) => value?.trim())
-    @MaxLength(100)
-    readonly title?: string;
+  @IsString()
+  @IsOptional()
+  @Transform(({ value }) => value?.trim())
+  @MaxLength(100)
+  readonly title?: string;
 
-    @IsString()
-    @IsOptional()
-    @Transform(({ value }) => value?.trim())
-    @MaxLength(50)
-    readonly isbn?: string;
+  @IsISBN()
+  @IsOptional()
+  readonly isbn?: string;
 
-    @IsDateString()
-    @IsOptional()
-    readonly publicationDate?: string;
+  @IsDateString()
+  @IsOptional()
+  readonly publishedDate?: string;
 
     @IsString()
     @IsOptional()
